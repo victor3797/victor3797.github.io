467d945f78b54911ebe353247330f9300bfb4402
victor3797 <31323847+victor3797@users.noreply.github.com>
2017-11-13 15:44:46 -0600

    edits made

--- Script1.js
+++ Script1.js
@@ -64,7 +64,7 @@ var slideInterval = setInterval(nextSlide,5000);
 
 function nextSlide()
 {
-    
+    slides[currentSlide].className = 'slide'
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slide showing';
 }
