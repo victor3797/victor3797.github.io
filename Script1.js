e80baa055a1deafce3b08f8b1124d451543fdcd9
victor3797 <31323847+victor3797@users.noreply.github.com>
2017-11-13 15:38:07 -0600

    edits made

--- Script1.js
+++ Script1.js
@@ -57,3 +57,14 @@ window.onclick = function(e) {
       }
   }
 }
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide()
+{
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
