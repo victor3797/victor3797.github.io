a8caceb4f73cc0f5a1add6d123ccbe89cb13bc33
victor3797 <31323847+victor3797@users.noreply.github.com>
2017-11-13 16:15:54 -0600

    editsmade

--- Script1.js
+++ Script1.js
@@ -76,7 +76,7 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "Dream Catcher.jpg";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
